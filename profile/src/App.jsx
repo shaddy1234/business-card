8676df91a0665c6af1ac1b530dca3c80280c1568
Shadrack Kimaau <musyokishadrach1@gmail.com>
2023-11-10 10:46:27 +0300

    Initial commit

---
+++ profile/src/App.jsx
@@ -0,0 +1,17 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <div className='contacts'>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ profile/src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+function Card() {
+
+
+  return (
+    <div className='contact-card'>
+      <div><img src="./images/person.png" alt="" /></div>
+      <h3>Kim Shaddy</h3>
+      <p><b>Frontend</b> Developer</p>
+      <div className='buttons'>
+        <a href='www.linkedin.com/in/shadrack-kimaau-8b84a4278
+
+        ' target="_blank"><button>LinkedIn</button></a>
+        <button>Twitter</button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
